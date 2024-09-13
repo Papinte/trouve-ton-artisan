8a24f0f4ad0d1325a2b39d054aa453dedcb5bbf4
Papinte <antoine.papin@hotmail.fr>
2024-09-13 14:59:50 +0200

    Modification du footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,38 +1,56 @@
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons';
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between py-3 my-4">
-        <ul className="nav col-md-4 justify-content-center d-flex">
-          <li className="ms-3">
-            <NavLink className="footer-link" to="/acces">
-              Accessibilité
-            </NavLink>
-          </li>
-          <li class="ms-3">
-            <NavLink className="footer-link" to="/legalNotice">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+      <ul className="list-unstyled">
+          <li className="nav-item mb-2">
+            <NavLink
+              to="/legalNotice"
+              className="footer-link"
+            >
               Mentions légales
             </NavLink>
           </li>
-          <li class="ms-3">
-            <NavLink className="footer-link" to="/cookies">
+          <li className="nav-item mb-2">
+            <NavLink
+              to="/personalData"
+              className="footer-link">
+              Données personnelles
+            </NavLink>
+          </li>
+          <li className="nav-item mb-2">
+            <NavLink to="/acces" className="footer-link">
+              Accessibilité
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/cookies"
+              className="footer-link"
+            >
               Cookies
             </NavLink>
           </li>
-          <li class="ms-3">
-            <NavLink className="footer-link" to="/personalData">
-              Données Personnelles
-            </NavLink>
-          </li>
         </ul>
-        <div className="col-md-4 d-flex align-items-center">
-          <address>
-            101 cours Charlemagne CS 20033 69269 LYON CEDEX 02 France +33 (0)4
-            26 73 40 00
-          </address>
-        </div>
+        <address>
+          Adresse :<br />
+          <hr />
+          <FontAwesomeIcon icon={faMapMarkerAlt} /> 101 cours Charlemagne<br />
+          CS 20033 69269<br />
+          LYON CEDEX 02 France<br />
+        </address>
+
+        <address
+        >
+          Contact :<br />
+          <hr />
+          <FontAwesomeIcon icon={faPhone} /> +33 (0)4 26 73 40 00
+        </address>
       </footer>
     </div>
   );
